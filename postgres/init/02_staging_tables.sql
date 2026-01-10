b17608e44c33f486164a25fc949108233050f322
Ghulam Azka Alim <ghulam@itsecasia.com>
2026-01-10 13:03:06 +0000

    Edit 02_staging_tables.sql

--- postgres/init/02_staging_tables.sql
+++ postgres/init/02_staging_tables.sql
@@ -20,7 +20,18 @@ CREATE INDEX IF NOT EXISTS idx_staging_wazuh_events_backfill_event_id
 CREATE INDEX IF NOT EXISTS idx_staging_wazuh_events_backfill_event_ts
   ON staging.wazuh_events_backfill(event_ts);
 
+CREATE TABLE IF NOT EXISTS staging.zeek_events_backfill (
+  LIKE bronze.zeek_events_raw
+);
+
+CREATE INDEX IF NOT EXISTS idx_staging_zeek_events_backfill_event_id
+  ON staging.zeek_events_backfill(event_id);
+CREATE INDEX IF NOT EXISTS idx_staging_zeek_events_backfill_event_ts
+  ON staging.zeek_events_backfill(event_ts);
+
 GRANT INSERT ON staging.suricata_events_backfill TO rw_writer;
 GRANT INSERT ON staging.wazuh_events_backfill TO rw_writer;
+GRANT INSERT ON staging.zeek_events_backfill TO rw_writer;
 GRANT SELECT ON staging.suricata_events_backfill TO rw_writer, etl_runner;
 GRANT SELECT ON staging.wazuh_events_backfill TO rw_writer, etl_runner;
+GRANT SELECT ON staging.zeek_events_backfill TO rw_writer, etl_runner;
