1e2aea8f2500bcb6590f4bdac126b7d4d9b09349
Ghulam Azka Alim <ghulam@itsecasia.com>
2026-01-10 12:44:12 +0000

    Edit psql_examples.sql

--- scripts/psql_examples.sql
+++ scripts/psql_examples.sql
@@ -1,5 +1,6 @@
 SELECT count(*) FROM bronze.suricata_events_raw;
 SELECT count(*) FROM bronze.wazuh_events_raw;
+SELECT count(*) FROM bronze.zeek_events_raw;
 SELECT count(*) FROM silver.security_events;
 SELECT * FROM gold.alerts_5m ORDER BY window_start DESC LIMIT 10;
 SELECT * FROM monitoring.lag_metrics ORDER BY observed_at DESC LIMIT 5;
