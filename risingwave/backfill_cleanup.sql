3f83d1f39c841bc053e3903f077002ba53417b31
Ghulam Azka Alim <ghulam@itsecasia.com>
2026-01-10 12:56:54 +0000

    Edit backfill_cleanup.sql

--- risingwave/backfill_cleanup.sql
+++ risingwave/backfill_cleanup.sql
@@ -1,5 +1,7 @@
 DROP SINK IF EXISTS suricata_backfill_sink;
 DROP SINK IF EXISTS wazuh_backfill_sink;
+DROP SINK IF EXISTS zeek_backfill_sink;
 DROP MATERIALIZED VIEW IF EXISTS suricata_backfill_mv;
 DROP MATERIALIZED VIEW IF EXISTS wazuh_backfill_mv;
+DROP MATERIALIZED VIEW IF EXISTS zeek_backfill_mv;
 DROP SOURCE IF EXISTS security_events_backfill_source;
