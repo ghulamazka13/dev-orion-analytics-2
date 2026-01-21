0606f6f2aa877a8735f1372d9a557d58af66bde1
Ghulam <azka.ghulam13@gmail.com>
2026-01-21 10:15:04 +0700

    commit

--- postgres/init/00_extensions.sql
+++ postgres/init/00_extensions.sql
@@ -1,1 +1,7 @@
-CREATE EXTENSION IF NOT EXISTS pg_duckdb;
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_available_extensions WHERE name = 'pg_duckdb') THEN
+    CREATE EXTENSION IF NOT EXISTS pg_duckdb;
+  END IF;
+END
+$$;
